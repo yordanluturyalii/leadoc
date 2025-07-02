35fc625b1d100fce7813ce77dc2ff707024cccd0
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-02 23:08:08 +0700

    :lipstick: ui: update button dark

--- features/shared/components/Button.tsx
+++ features/shared/components/Button.tsx
@@ -13,11 +13,11 @@ type ButtonProps = {
 
 const Button = ({text, icon, isDisable, isDark, handleClick}: ButtonProps) => {
   return (
-    <button type='button' className={`bg-white w-full h-11 flex items-center justify-center gap-2.5 cursor-pointer border border-neutral-200 rounded-xl hover:bg-neutral-50 ${isDark && 'bg-neutral-900 text-white'}`} onClick={handleClick}>
+    <button type='button' className={`${isDark ? 'bg-neutral-900' : 'bg-white border border-neutral-200 hover:bg-neutral-50'}  w-full h-11 flex items-center justify-center gap-2.5 cursor-pointer rounded-xl`} onClick={handleClick}>
         {
             icon && <Image src={icon} alt='Github Icon' width={24} height={24} />
         }
-        <span className='text-body-md font-semibold text-neutral-900 '>{text}</span>
+        <span className={`text-body-md font-semibold ${isDark ? "text-white" : "text-neutral-900" }`}>{text}</span>
     </button>
   )
 }
