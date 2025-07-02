3c4495e2675d24e27a5f56e70ad2f8b1e72e8145
yordanluturyalii <yordanluturyali@gmail.com>
2025-07-02 22:17:46 +0700

    :label: feat: add register type

---
+++ features/auth/types/register-types.ts
@@ -0,0 +1,18 @@
+import { z, ZodType } from "zod";
+
+export type RegisterData = {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string
+};
+
+export const RegisterSchema: ZodType<RegisterData> = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+    passwordConfirmation: z.string().min(8)
+}).refine(({passwordConfirmation, password}) => password === passwordConfirmation, {
+    message: "The password confirmation does not match.",
+    path: ["passwordConfirmation"]
+});
