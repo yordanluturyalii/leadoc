a21634fb29d2d727d393dae2952ca24a4c550415
algazza <fadhilaswadira72564@gmail.com>
2025-07-15 20:53:35 +0700

    :lipstick: ui: update ui for login page

--- features/auth/components/login/Login.tsx
+++ features/auth/components/login/Login.tsx
@@ -11,43 +11,59 @@ const Login = () => {
     window.location.replace('http://localhost:3001/api/auth')
   }
   return (
-      <div className='bg-neutral-50 border-[1px] border-neutral-200 md:p-20 rounded-3xl p-5'>
-        <div className="mb-10">
-          <Image src={'/leadoc-logo.png'} alt='Leadoc' width={110} height={24}/>
-        </div>
-        <div className="mb-8">
-          <h1 className="text-title-md text-neutral-900">Create account</h1>
-          <p className="text-body-md text-neutral-500">Turn your repository into clean documentation with just one
-            click</p>
-        </div>
+      <div className="flex justify-between w-full h-full gap-2.5">
+        <div className='w-full lg:w-1/2 h-full bg-neutral-50 border-[1px] border-neutral-200 md:p-20 rounded-3xl p-5'>
+          <div className="mb-10">
+            <Image src={'/leadoc-logo.png'} alt='Leadoc' width={110} height={24}/>
+          </div>
+          <div className="mb-8">
+            <h1 className="text-title-md text-neutral-900">Create account</h1>
+            <p className="text-body-md text-neutral-500">Turn your repository into clean documentation with just one
+              click</p>
+          </div>
 
-        <Button icon='/github.png' type='button' isDisable={false} isDark={false} handleClick={handleGithubAuth}>
-          Continue With Github
-        </Button>
+          <Button icon='/github.png' type='button' isDisable={false} isDark={false} handleClick={handleGithubAuth}>
+            Continue With Github
+          </Button>
 
-        <div className="my-6 w-full flex items-center gap-3">
-          <span className="text-body-md text-neutral-500">Continue with Email</span>
-          <div className="flex-1 h-px bg-neutral-200"/>
-        </div>
+          <div className="my-6 w-full flex items-center gap-3">
+            <span className="text-body-md text-neutral-500">Continue with Email</span>
+            <div className="flex-1 h-px bg-neutral-200"/>
+          </div>
 
-        <LoginForm/>
+          <LoginForm/>
 
-        <div className="my-6">
+          <div className="my-6">
           <span className="text-body-md text-neutral-500">By creating an account you agree to our {' '}
             <Link href='/' className='text-purple-600'>Terms of Service</Link>
             {' '} and {' '}
             <Link href='/' className='text-purple-600'>Privacy Policy</Link>
           </span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-body-md text-neutral-500">Already have an account?</span>
+            <div className="w-16">
+              <Button type='button' isDisable={false} isDark={false} handleClick={() => {
+                console.log('hai')
+              }}>
+                Sign In
+              </Button>
+            </div>
+          </div>
         </div>
 
-        <div className="flex items-center gap-2">
-          <span className="text-body-md text-neutral-500">Already have an account?</span>
-          <div className="w-16">
-            <Button type='button' isDisable={false} isDark={false} handleClick={() => {
-              console.log('hai')
-            }}>
-              Sign In
-            </Button>
+        <div className="hidden w-1/2 lg:flex flex-col justify-end rounded-3xl p-6">
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center gap-1">
+              <span className="text-title-xl font-semibold text-neutral-900">4.8</span>
+              <span className="text-title-xxs font-semibold text-neutral-500">/5</span>
+            </div>
+            <span className="w-full text-title-xxs text-neutral-600">
+              "As a developer, I struggled to keep documentation updated with repository changes. Since using this GitHub automation tool, my workflow is more efficient. Every pull request generates clean, updated docs automatically. I can focus on building features instead of formatting documentation. It saves time and keeps our docs consistent."
+            </span>
+            <span
+                className="text-body-xl text-neutral-900 font-semibold">John Doe, Backend Engineer @Interland.co</span>
           </div>
         </div>
       </div>
