4f056ec68e166efbea396cbadf1b116a1bcbd681
algazza <fadhilaswadira72564@gmail.com>
2025-07-16 13:29:55 +0700

    :recycle: refactor: refactor layout for background

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-      <main className="w-full bg-neutral-100 p-5">
+      <main className="w-full p-5">
         {children}
       </main>
   )
