6fd711ccfdff793e0c582ece2ba88f5a9ccc6705
algazza <fadhilaswadira72564@gmail.com>
2025-07-16 20:48:37 +0700

    :sparkles: feat: update login form with react-hooks-form function

--- features/auth/components/login/LoginForm.tsx
+++ features/auth/components/login/LoginForm.tsx
@@ -3,19 +3,38 @@
 import React, { FormEvent } from 'react';
 import Input from '@/features/shared/components/Input';
 import Button from '@/features/shared/components/Button';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LoginData, LoginSchema } from '@/features/auth/types/login-types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
 
 const LoginForm = () => {
-  const handleSubmit = () => {
-    console.log('handleSubmit');
-  }
+  const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: {errors, disabled, isLoading, isValid},
+    setError,
+  } = useForm<LoginData>({
+    resolver: zodResolver(LoginSchema)
+  })
+  const onSubmit: SubmitHandler<LoginData> = (data) => console.log(data)
 
   return (
-      <form className='flex flex-col gap-4'>
-        <Input label='Email' name='email' type='email'/>
-        <Input label='Password' name='password' type='password'/>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
+        <Input
+            label='Email'
+            {...register('email')}
+            error={errors.email?.message}
+        />
+        <Input
+            label='Password'
+            {...register('password')}
+            error={errors.password?.message}
+        />
 
         <div className="mt-2">
-          <Button type='submit' isDisable={false} isDark={true} handleClick={handleSubmit}>
+          <Button type='submit' isDisable={!isValid || isLoading || disabled} isDark={true}>
             Continue
           </Button>
         </div>
