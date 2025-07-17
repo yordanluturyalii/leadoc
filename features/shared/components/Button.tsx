d08833239772220d272d9ded42f3823220445ad5
algazza <fadhilaswadira72564@gmail.com>
2025-07-17 19:23:56 +0700

    :lipstick: ui: update button component

--- features/shared/components/Button.tsx
+++ features/shared/components/Button.tsx
@@ -16,7 +16,7 @@ const Button = ({children, icon, type, isDisable, isDark, handleClick}: ButtonPr
   return (
       <button
           type={type}
-          className={`${isDark ? 'bg-neutral-900' : 'bg-white border border-neutral-200 hover:bg-neutral-50'} w-full p-2 flex items-center justify-center gap-2.5 cursor-pointer rounded-xl`}
+          className={`${isDark ? 'bg-neutral-900' : 'bg-white border border-neutral-200 hover:bg-neutral-50'} w-full p-3 flex items-center justify-center gap-2.5 cursor-pointer rounded-xl`}
           onClick={handleClick}>
         {
             icon && <Image src={icon} alt='Github Icon' width={24} height={24} />
