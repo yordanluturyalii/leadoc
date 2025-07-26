a8b0c5b7cacd1b3b3b8eed355a58b69d6f443255
algazza <fadhilaswadira72564@gmail.com>
2025-07-26 17:51:09 +0700

    :lipstick: ui: add animation opacity

--- features/marketing/component/Header.tsx
+++ features/marketing/component/Header.tsx
@@ -3,8 +3,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { ChevronDown, ChevronUp, Menu, X } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { socialMedia } from '@/features/marketing/lib/constant';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const Header = () => {
   const [menu, setMenu] = useState<boolean>(false)
@@ -69,7 +74,7 @@ const Header = () => {
               </div>
             </nav>
 
-            <HeaderHamburger menu={menu} company={company} companyHandler={companyHandler}/>
+            <HeaderHamburger isVisible={menu} company={company} companyHandler={companyHandler}/>
           </div>
 
         </header>
@@ -121,71 +126,69 @@ const HeaderHover = () => {
   )
 }
 
-const HeaderHamburger = ({ menu, company, companyHandler }: {
-  menu: boolean,
+const HeaderHamburger = ({ isVisible, company, companyHandler }: {
+  isVisible: boolean,
   company: boolean,
   companyHandler: () => void
 }) => {
+  useGSAP(() => {
+    if (isVisible) {
+      gsap.to('.hamburger', {
+        opacity: 1,
+        y: 0,
+        pointerEvents: 'auto',
+        duration: 0.2,
+        ease: 'power2.out',
+      })
+    } else {
+      gsap.timeline().to('.hamburger', {
+        opacity: 0,
+        pointerEvents: 'none',
+        duration: 0.2,
+        ease: 'power2.in',
+      }).to('.hamburger', {
+        y: -30,
+        duration: 0.2,
+        delay: 0.4
+      })
+    }
+  }, [isVisible])
+
   return (
       <div
-          className={`flex flex-col justify-between p-10 transition-opacity duration-700 xl:hidden overflow-hidden ease-in-out ${
-              menu ? 'h-[92dvh] opacity-100 visible' : ' opacity-0 invisible hidden'
-          }`}>
+          style={{ opacity: 0, pointerEvents: 'none' }}
+          className="hamburger fixed top-[64px] left-0 w-full h-[calc(100dvh-64px)] bg-white z-40 flex flex-col justify-between p-10 transition-opacity duration-700 xl:hidden overflow-hidden ease-in-out"
+      >
         <ul className="flex flex-col gap-4">
-          <li className="">
-            <Link href='/'>
-              Features
-            </Link>
-          </li>
-
-          <div className='bg-neutral-100 h-px w-full'/>
-
-          <li className="">
-            <Link href='/'>
-              Pricing
-            </Link>
-          </li>
-
-          <div className='bg-neutral-100 h-px w-full'/>
-
-          <button
-              type='button'
-              onClick={companyHandler}
-          >
-            <div
-                className="w-full flex justify-between items-center"
-            >
+          <li><Link href='/'>Features</Link></li>
+          <div className='bg-neutral-100 h-px w-full' />
+          <li><Link href='/'>Pricing</Link></li>
+          <div className='bg-neutral-100 h-px w-full' />
+          <button onClick={companyHandler} type='button'>
+            <div className="w-full flex justify-between items-center">
               <span>Company</span>
               {company ? <ChevronUp color="#171717"/> : <ChevronDown color="#171717"/>}
             </div>
-
             <div className={`grid overflow-hidden transition-all duration-300 ${
                 company ? 'grid-rows-[1fr] opacity-100 mt-7' : 'grid-rows-[0fr] opacity-0'
             }`}>
               <div className="text-left overflow-hidden text-neutral-600 font-medium flex flex-col gap-3">
-                <Link className='overflow-hidden' href='/abouhait'>About</Link>
-                <Link className='overflow-hidden' href='/about'>Contact</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/contact'>Contact</Link>
               </div>
             </div>
           </button>
-
-          <div className='bg-neutral-100 h-px w-full'/>
-
-          <li className="">
-            <Link href='/'>
-              Changelog
-            </Link>
-          </li>
+          <div className='bg-neutral-100 h-px w-full' />
+          <li><Link href='/'>Changelog</Link></li>
         </ul>
 
         <div className="flex flex-col gap-4 w-full text-center font-semibold">
-          <Link href='/login' className="py-2 border border-neutral-200 rounded-lg">
-            Sign in
-          </Link>
+          <Link href='/login' className="py-2 border border-neutral-200 rounded-lg">Sign in</Link>
           <Link href='/signup' className="py-2 bg-neutral-900 rounded-lg text-white">Sign Up</Link>
         </div>
       </div>
   )
 }
+
 
 export default Header;
