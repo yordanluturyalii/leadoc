b006dc2d0e966b597bd79c0f46ad0e919c85e895
algazza <fadhilaswadira72564@gmail.com>
2025-07-26 23:03:19 +0700

    :recycle: refactor: changes name file

---
+++ features/marketing/component/FadingSection.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import gsap from 'gsap';
+import { animationSection } from '@/features/marketing/lib/constant';
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+export const FadingSection = ({ section, children }: { section: string, children: React.ReactNode }) => {
+  useGSAP(() => {
+    const titleSplit = new SplitText(`.${section}`, { type: 'chars, words' })
+
+    gsap.from(titleSplit.chars, {
+      opacity: 0,
+      // filter: 'blur(10px)',
+      stagger: 0.01,
+      scrollTrigger: {
+        trigger: `.${section}-section`,
+        start: 'top top',
+        end: '+=1000',
+        scrub: true,
+        pin: true,
+      },
+    })
+
+    animationSection.forEach(({ trigger, startValue, bg, revert }) => {
+      ScrollTrigger.create({
+        trigger: trigger,
+        start: startValue,
+        onEnter: () => {
+          gsap.to('main, header', {
+            backgroundColor: bg,
+            duration: 1.5,
+          })
+        },
+        onLeaveBack: () => {
+          gsap.to('main, header', {
+            backgroundColor: revert,
+            duration: 1.5,
+          })
+        },
+      })
+    })
+  }, [])
+
+  return (
+      <section
+          className={`${section}-section h-screen px-5 flex flex-col items-center justify-center gap-9 text-center md:px-10`}>
+        {children}
+      </section>
+  );
+};
--- features/marketing/component/TitleGradient.tsx
+++ features/marketing/component/TitleGradient.tsx
@@ -2,7 +2,8 @@
 
 import React from 'react';
 
-export const TitleGradient = ({ children, className }: { children: React.ReactNode, className: string }) => {
+export const
+    TitleGradient = ({ children, className }: { children: React.ReactNode, className: string }) => {
   return (
       <span
           className={`py-1 px-6 w-fit bg-gradient-to-r text-center font-medium rounded-sm md:py-1.5 ${className}`}
