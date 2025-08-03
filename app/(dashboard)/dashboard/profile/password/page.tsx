c9c7157713afac7f6109da99b5f852560e84f903
yordanluturyalii <yordanluturyali@gmail.com>
2025-08-03 22:10:31 +0700

    :lipstick: ui: add ui password page

---
+++ app/(dashboard)/dashboard/profile/password/page.tsx
@@ -0,0 +1,9 @@
+import UpdatePassword from '@/features/profile/components/Password/UpdatePassword'
+
+const UpdatePasswordPage = () => {
+    return (
+        <UpdatePassword />
+    )
+}
+
+export default UpdatePasswordPage
