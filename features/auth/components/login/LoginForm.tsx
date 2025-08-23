d9321551d9bde068280e604798442e7d75709d41
algazza <fadhilaswadira72564@gmail.com>
2025-08-23 21:13:19 +0700

    :sparkles: feat: add function for api

--- features/auth/components/login/LoginForm.tsx
+++ features/auth/components/login/LoginForm.tsx
@@ -1,4 +1,4 @@
- "use client";
+"use client";
 
 import React, { FormEvent } from 'react';
 import Input from '@/features/shared/components/Input';
@@ -7,18 +7,55 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { LoginData, LoginSchema } from '@/features/auth/types/login-types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
+import { api } from '@/features/shared';
+
+``
+const loginUser = async (data: any) => {
+  const response = await api.post("/api/auth/login", data);
+  return response.data;
+}
 
 const LoginForm = () => {
-  const router = useRouter()
+  const route = useRouter()
   const {
     register,
     handleSubmit,
-    formState: {errors, disabled, isLoading, isValid},
+    formState: { errors, disabled, isLoading, isValid },
     setError,
   } = useForm<LoginData>({
     resolver: zodResolver(LoginSchema)
   })
-  const onSubmit: SubmitHandler<LoginData> = (data) => console.log(data)
+
+  const mutation = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      route.push('/dashboard');
+    },
+    onError: (error) => {
+      if (error?.errors && Object.keys(error?.errors).length > 0) {
+        Object.entries(error?.errors).forEach(([index, { type, message }]: [string, any]) => {
+          const fieldMap: { [key: string]: keyof LoginData } = {
+            'email': 'email',
+            'password': 'password',
+          };
+
+          const formField = fieldMap['email'];
+          console.log(formField);
+          if (formField) {
+            setError(formField, { message });
+          }
+        });
+      }
+    }
+  })
+
+  const onSubmit: SubmitHandler<LoginData> = (data) => {
+    mutation.mutate({
+      email: data.email,
+      password: data.password,
+    });
+  }
 
   return (
       <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
@@ -34,9 +71,8 @@ const LoginForm = () => {
             error={errors.password?.message}
             isForgotPassword={true}
         />
-
         <div className="mt-2">
-          <Button type='submit' isDisable={false} isDark={true} className='h-[44px] w-full'>
+          <Button type='submit' isDisable={!isValid || isLoading || disabled} isDark={true} className='h-[44px] w-full'>
             Continue
           </Button>
         </div>
