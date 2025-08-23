a06c8725b114bbc0d769bb39c72acccec85f0650
algazza <fadhilaswadira72564@gmail.com>
2025-08-23 21:18:59 +0700

    :sparkles: feat: add forgot-password page

---
+++ features/auth/components/forgot-password/ForgotPasswordForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { FormEvent, useState } from 'react';
+import Input from '@/features/shared/components/Input';
+import Button from '@/features/shared/components/Button';
+import Toast from '@/features/shared/components/Toast';
+import { TriangleAlert } from 'lucide-react';
+
+const ForgotPasswordForm = () => {
+  const [isToast, setIsToast] = useState<boolean>(false)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsToast(true);
+  }
+
+  return (
+      <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
+        {isToast && (
+            <Toast type='success'>
+              <TriangleAlert/> We’ll email you a reset link if the address is registered.
+            </Toast>
+        )}
+
+        <Input
+            label='Email'
+            name='email'
+        />
+
+        <div className="mt-2">
+          <Button type='submit' isDisable={false} isDark={true} className='w-full h-[44px]'>
+            Send Link
+          </Button>
+        </div>
+      </form>
+  );
+};
+
+export default ForgotPasswordForm;
