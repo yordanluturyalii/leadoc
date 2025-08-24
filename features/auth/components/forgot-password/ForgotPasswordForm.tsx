67f6e52593d1d388bd5beda1af14ecccb51f9e17
algazza <fadhilaswadira72564@gmail.com>
2025-08-24 09:23:27 +0700

    ✨ feat: add api for send email

--- features/auth/components/forgot-password/ForgotPasswordForm.tsx
+++ features/auth/components/forgot-password/ForgotPasswordForm.tsx
@@ -1,38 +1,77 @@
 "use client";
 
-import React, { FormEvent, useState } from 'react';
-import Input from '@/features/shared/components/Input';
-import Button from '@/features/shared/components/Button';
-import Toast from '@/features/shared/components/Toast';
-import { TriangleAlert } from 'lucide-react';
+import React, { useState } from "react";
+import Input from "@/features/shared/components/Input";
+import Button from "@/features/shared/components/Button";
+import Toast from "@/features/shared/components/Toast";
+import { TriangleAlert } from "lucide-react";
+import { api } from "@/features/shared";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+
+const ForgotSchema = z.object({
+  email: z.string().email(),
+});
+
+type ForgotPassword = z.infer<typeof ForgotSchema>;
+
+const forgotPassword = async (data: ForgotPassword) => {
+  const response = await api.post("/api/auth/send-email", data);
+  return response.data;
+};
 
 const ForgotPasswordForm = () => {
-  const [isToast, setIsToast] = useState<boolean>(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, disabled, isLoading, isValid },
+    setError,
+  } = useForm<ForgotPassword>({
+    resolver: zodResolver(ForgotSchema),
+  });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsToast(true);
-  }
+  const mutation = useMutation({
+    mutationFn: forgotPassword,
+    onError: (err: any) => {
+      const errors = err?.response?.data?.errors;
+      if (errors?.email) {
+        setError("email", { message: errors.email.message });
+      }
+    },
+  });
+
+  const onSubmit: SubmitHandler<ForgotPassword> = async (data) => {
+    mutation.mutate(data);
+  };
 
   return (
-      <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
-        {isToast && (
-            <Toast type='success'>
-              <TriangleAlert/> We’ll email you a reset link if the address is registered.
-            </Toast>
-        )}
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+      {mutation.isSuccess && (
+        <Toast type="success">
+          <TriangleAlert /> We’ll email you a reset link if the address is
+          registered.
+        </Toast>
+      )}
 
-        <Input
-            label='Email'
-            name='email'
-        />
+      <Input
+        label="Email"
+        {...register("email")}
+        error={errors.email?.message}
+      />
 
-        <div className="mt-2">
-          <Button type='submit' isDisable={false} isDark={true} className='w-full h-[44px]'>
-            Send Link
-          </Button>
-        </div>
-      </form>
+      <div className="mt-2">
+        <Button
+          type="submit"
+          isDisable={isLoading || !isValid || disabled}
+          isDark={true}
+          className="w-full h-[44px]"
+        >
+          Send Link
+        </Button>
+      </div>
+    </form>
   );
 };
 
