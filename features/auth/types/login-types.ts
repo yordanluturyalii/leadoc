72644edeb13069208d0fed7f70a36756dd102bfa
algazza <fadhilaswadira72564@gmail.com>
2025-08-24 16:54:40 +0700

    ⚡️ feat: add custom hooks for email

--- features/auth/types/login-types.ts
+++ features/auth/types/login-types.ts
@@ -1,11 +1,8 @@
 import { z } from 'zod';
 
-export type LoginData = {
-  email: string
-  password: string
-}
-
 export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8)
 })
+
+export type LoginData = z.infer<typeof LoginSchema>
